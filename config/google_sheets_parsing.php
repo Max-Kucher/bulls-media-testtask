9fd16f4dd9bb74f0dd60313b404ced2d6dd0de3f
Max-Kucher <mkuchirov@gmail.com>
2023-03-21 15:58:33 +0200

    Added one more sheet for test

--- config/google_sheets_parsing.php
+++ config/google_sheets_parsing.php
@@ -74,8 +74,65 @@ return [
             ],
         ],
 
-//        1256509563 => [
-//            'db_table' => 'products',
-//        ],
+        1256509563 => [
+            'db_table' => 'products',
+            'fields' => [
+                'date123' => [
+                    'db_field' => 'date',
+                    'type' => 'integer',
+                    'date_format' => 'd.m.Y',
+
+                    /**
+                     * Additional modifiers for column
+                     */
+                    'additional_modifiers' => [
+                        'unsigned', 'nullable'
+                    ],
+                ],
+                'product_name123' => [
+                    'db_field' => 'name',
+                    'type' => 'string',
+                    'type_params' => [100],
+                    'additional_modifiers' => [
+                        'nullable'
+                    ],
+                ],
+                'price' => [
+                    'db_field' => 'price',
+                    'type' => 'unsignedDecimal',
+                    'type_params' => [8, 2],
+                    'additional_modifiers' => [
+                        'nullable'
+                    ],
+                ],
+                'amount' => [
+                    'db_field' => 'stocks',
+                    'type' => 'integer',
+                    'additional_modifiers' => [
+                        'nullable'
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'https://docs.google.com/spreadsheets/d/1-TMK2G_wpzvczCqFDKVYm97AqvuCa9DLUdgpjur2QBg' => [
+        0 => [
+            'db_table' => 'sales',
+            'fields' => [
+                'sale_id' => [
+                    'db_field' => 'sale_id',
+                    'type' => 'increments',
+                ],
+                'amount' => [
+                    'db_field' => 'sale_amount',
+                    'type' => 'unsignedDecimal',
+                    'type_params' => [8, 2],
+                    'additional_modifiers' => [
+                        'nullable'
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
